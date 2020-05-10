7c15509cc61241c3266e60b637e1a5b24b5f6510
tungtrana3 <52845932+tungtrana3@users.noreply.github.com>
2020-05-10 11:52:46 +0700

    add project

---
+++ handleAccountJwt.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+exports.getAccountId = (req, res) => {
+    if (req.token === 'admin') {
+        return 0;
+    }
+    return jwt.verify(req.token, 'jwt-secret', (err, data) => {
+        if (err) {
+            console.log(err)
+            res.json({
+                resultCode: -1,
+                message: 'Không tìm thấy người dùng này',
+                data: null,
+            })
+        } else {
+            return data.id
+        }
+    })
+}
